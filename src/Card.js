01f5745b63237dc8cb105e1c75ebca8e9370d6da
amber-tamara <ambertjansen@gmail.com>
2020-03-07 12:23:30 +0000

    robofriends

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+//destructering - Allows us to grab the props object and its properties.
+const Card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+export default Card;
